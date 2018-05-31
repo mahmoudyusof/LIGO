771ac0e1a258b71f87c52b16b69655cc3e9017c0
Mahmoud Youssef <mahmoud.yusof27@gmail.com>
2018-05-31 14:16:17 +0200

    i am back to boiler plate

--- app/controllers/WelcomeController.php
+++ app/controllers/WelcomeController.php
@@ -1,9 +1,7 @@
 <?php
 
 namespace App\Controllers;
-use App\Models\User;
 use App\Core\Form;
-use App\Core\Request;
 
 class WelcomeController
 {
@@ -15,30 +13,6 @@ class WelcomeController
     public function greet($name)
     {
         echo app()->render("greet", $name=$name);
-    }
-    public function show()
-    {
-        $users = User::where("id", "<", "3")->get();
-        echo app()->render("show", compact("users"));
-    }
-
-    public function add()
-    {
-        echo app()->render("add");
-    }
-
-    public function create()
-    {
-        // fix CSRF later.
-        
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $user = User::create([
-            "name" => $name,
-            "email" => $email
-        ]);
-        $user->save();
-        header('location: /');
     }
 }
 
